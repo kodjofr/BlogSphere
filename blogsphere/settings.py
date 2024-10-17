99fe89715873060114f2f87746690beb244fbd3d
kodjofr <francoisxegbe@gmail.com>
2024-10-17 15:32:20 +0100

    fix: Remove Postgresql config

--- blogsphere/settings.py
+++ blogsphere/settings.py
@@ -67,14 +67,11 @@ WSGI_APPLICATION = 'blogsphere.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
